862de6b3bbd6df56d596e1e02e8a43af0dc6a5b6
Pablo Ibarz <63760047+Roboticy3@users.noreply.github.com>
2022-02-25 12:57:34 -0500

    Added comments to BoardConverter.gd

--- Objects/Board/BoardConverter.gd
+++ Objects/Board/BoardConverter.gd
@@ -1,12 +1,18 @@
 class_name BoardConverter
-#static class that handles global board stuff
+
+#BoardConverter Class made by Pablo Ibarz
+#created in December 2021
+
+#BoardConverter started as a converter for things related to well, the board
+#but now its a more general use collection of static functions because I'm too lazy to learn how to use a singleton
 
 #path to the default mesh
 static func default_mesh():
 	return "res://Meshes/default.obj"
 
 #return the face index, position in space and normal of mesh from a uv coordinate pos
-#optionally send in a mask of face indices to exclude (mask_type == 0) or include (mask_type == 1) 
+#optionally send in a mask of face indices to exclude (mask_type == 0), include (mask_type == 1), or search first (mask_type == 2)
+#TODO make search first into separate function which searches faces recursively from a starting face in mask through its connected faces
 static func uv_to_mdata(var mdt:MeshDataTool, var pos:Vector2 = Vector2.ZERO, 
 	var mask:Array = [], var mask_type:int = 2):
 	
@@ -14,21 +20,27 @@ static func uv_to_mdata(var mdt:MeshDataTool, var pos:Vector2 = Vector2.ZERO,
 	if pos == null: 
 		return null
 	
-	#integer array to convert from shortened index to real index in mdt
+	#integer array to convert from index to real index in mdt using mask
+	#not setting anything to true_indices will lead to it being ignored in favor of regular ones
 	var true_indices = []
 	var size:int = 0
 	
 	#mask type 2 iterates through verts in the mask first
 	if mask_type == 2:
+		#same number of iterations as face count because every face can be searched eventually
 		size = mdt.get_face_count()
+		#check for indices of mdt faces in mask, and add them to the front of true indices
+		#this is slower for larger masks because push_front causes a lot of re-indexing
 		for i in mdt.get_face_count():
 			if i in mask:
 				true_indices.push_front(i)
 			else:
 				true_indices.append(i)
-	#ignore empty masks in other modes
+		
+	#ignore empty masks in other modes, even if mask_type == 1 and mask.empty() should cause and empty loop
 	elif mask.empty(): 
 		size = mdt.get_face_count()
+	
 	#otherwise, create a mask
 	else:
 		#mask type 0 excludes verts in the mask
@@ -42,38 +54,42 @@ static func uv_to_mdata(var mdt:MeshDataTool, var pos:Vector2 = Vector2.ZERO,
 			size = mask.size()
 			true_indices = mask
 	
+	#convert pos into Vector3 with y=0 to be compatible with Triangle's Vector3 components
+	var p:Vector3 = Vector3(pos.x, 0, pos.y)
+	
 	for j in size:
-		#if size is the same as the face count, set i to j
+		#assume size is the same as face count
 		var i = j
-		#otherwise there is a mask, and i must take from true_indices
+		#if j fits in true_indices, reassign i to the true index
 		if j < true_indices.size(): 
 			i = true_indices[j]
 		
+		#get positions and uvs of the triangle
 		var t = get_face_vertices(mdt, i)
 		#copy positions of t into another triangle to calculate position later
 		var u = t[0]
+		
 		#convert uvs into Vector3 with y=0
 		for k in t[1].size():
 			t[1][k] = Vector3(t[1][k].x, 0, t[1][k].y)
-			
+		#send uvs expanded into 3D into t
 		t = Triangle.new(t[1])
 		
-		#check if triangle is surrounding uv position
-		#do same vec3 conversion with uv position
-		var p = Vector3(pos.x, 0, pos.y)
+		#check if the right triangle has been found
 		if t.is_surrounding(p):
-			#if the right triangle has been found,
-			#get the barycentric coordinates of th uv coordinate
+			#get the barycentric coordinates of p in t
 			var b = t.barycentric(p)
 			#then use the bcoords to weigh the positions of the tri's verts into an average for output
+			#this is why the position copy was made earlier
 			var out = b[0] * u[0] + b[1] * u[1] + b[2] * u[2]
-			#if mask mode is 2, add surrounding faces to mask for faster proximity search
+			#if mask mode is 2, add surrounding faces to mask so the same mask so other functions can use the same mask multiple times for faster search
 			if mask_type == 2:
 				var faces:PoolIntArray = []
 				for v in range(0, 3):
 					faces.append_array(mdt.get_vertex_faces(mdt.get_face_vertex(i, v)))
 				mask.append_array(faces)
 			mask.append(i)
+			#return data about the triangle
 			return [i, out, mdt.get_face_normal(i), j]
 	
 	#if no solution is found, return null
@@ -88,7 +104,7 @@ static func get_face_vertices(var mdt:MeshDataTool, var i:int):
 	mdt.get_vertex_uv(mdt.get_face_vertex(i, 1)),
 	mdt.get_vertex_uv(mdt.get_face_vertex(i, 2))]]
 
-#convert from uv coordinate to square using the size of the board
+#convert from uv coordinate to square using the size in squares of the board
 static func uv_to_square(var size:Vector2, var pos):
 	if pos == null: return null
 	var large = pos * size
@@ -99,25 +115,28 @@ static func uv_to_square(var size:Vector2, var pos):
 #convert from square on the board to uv coordinates
 static func square_to_uv(var size:Vector2 = Vector2.ONE, var pos:Vector2 = Vector2.ZERO):
 	var square = Vector2.ONE/size
-	#put pos through the same adjustment as size to respect uv coords
+	#add 1 to pos to adjust for board starting at 0, 0
 	#subtract one half of the square size to get the uv coordinate in the center of the target square
 	return (pos + Vector2.ONE)/(size) - square/2
 
-#combine space converters into single funcions
+#combine space converters into single function
 static func square_to_mdata(var mdt:MeshDataTool = null, var size:Vector2 = Vector2.ONE, var pos:Vector2 = Vector2.ZERO):
 	var uv = square_to_uv(size, pos)
 	return uv_to_mdata(mdt, uv)
 
-#take an input board mdt, aabb, and piece to return a Transform for the associated PieceMesh
+#take an input board mdt, board, and piece to return a Transform for the associated PieceMesh accosiated with piece on the mdt constructed from a BoardMesh
 static func square_to_transform(var mdt:MeshDataTool, 
 	var board:Board, var piece:Piece):
 	
+	#reference useful piece properties in other variables
 	var pos:Vector2 = piece.pos
 	var table:Dictionary = piece.table
 	
+	#create a new transform to modify
 	var transform:Transform = Transform()
 	
 	#get mesh data on square center for position and normal of the piece
+	#if the piece is not centered on its origin, offsets can be created
 	var mdata = square_to_mdata(mdt, board.size, pos)
 	
 	#ROTATION
@@ -127,10 +146,11 @@ static func square_to_transform(var mdt:MeshDataTool,
 		transform.basis = square_to_basis(mdt, board, piece, mdata)
 		
 	#SCALE
+	#scale does not need the mdata step, but has to be executed after rotation
 	#if scale mode is 0, scale piece by board's piece scale param
 	if table["scale_mode"] == 0:
 		transform = transform.scaled(Vector3.ONE * board.table["piece_scale"])
-	#if scale mode is 1, dynamically scale piece, this is left up to PieceMesh
+	#TODO if scale mode is 1, dynamically scale piece, this is left up to PieceMesh
 	#if scale mode is 2, ignore scaling
 	
 	#TRANSLATION
@@ -140,17 +160,18 @@ static func square_to_transform(var mdt:MeshDataTool,
 	
 	return transform
 
-static func square_to_basis(var mdt:MeshDataTool, var board:Board, var piece:Piece, var mdata:Array):
-	#create directional vectors for the piece on its current square
+#convert the normal of a square on the board to a set of basis vectors
+static func square_to_basis(var mdt:MeshDataTool, var board:Board, 
+	var piece:Piece, var mdata:Array):
+	
 	#up vector will take the normal of the square
 	var up:Vector3 = mdata[2].normalized()
 	
-	#forward vector will look at square in forward direction from piece
-	#check if piece's direction goes out of bounds, if so try orthogonal direction
+	#forward vector will try to look at square in forward direction from piece, and use the relative directions to form a vector
 	var v:Vector2 = piece.get_forward()
 	var d = 0
+	#check if piece's direction goes out of bounds, if so try next orthogonal direction
 	#keep trying orthogonal directions until a valid spot is found
-	#keep track of direction in d
 	while !board.is_surrounding(piece.pos + v) && d < 4:
 		v = v.tangent()
 		d += 1
@@ -160,12 +181,14 @@ static func square_to_basis(var mdt:MeshDataTool, var board:Board, var piece:Pie
 		mf = square_to_mdata(mdt, board.size, piece.pos + v)
 	
 	#process mf[1] into a vector that is orthogonal to up
+	#these vectors are not necesarily orthogonal, but a true forward can be computed from up and right later
 	var fd:Vector3 = (mf[1] - mdata[1]).normalized()
 	
 	#last basis vector is cross of up and forward
 	var rt = up.cross(fd)
 	
 	#to force fd to be orthogonal to up, rotate fd by pi - angle between fd and up
+	#couldn't it just get the cross again?
 	var a = acos(fd.dot(up))
 	#cursedmathgames
 	fd = rt.rotated(up, PI/2)
@@ -191,6 +214,7 @@ static func square_to_child(var parent:Node, var mdt:MeshDataTool, var size:Vect
 
 #WIP return a convex cube mesh bounding a square
 static func square_to_box(var mdt:MeshDataTool, var size:Vector2, var square:Vector2=Vector2.ZERO,
+	#node input is only used for debugging, usually ignore
 	var node:Node = null):
 		
 	#get uv bound of square
@@ -227,6 +251,7 @@ static func square_to_box(var mdt:MeshDataTool, var size:Vector2, var square:Vec
 				var u:int = mdt.get_edge_vertex(e, 1)
 				var uv2:Vector2 = mdt.get_vertex_uv(u)
 				
+				#cursedmathgames
 				var cross = b.edge_set_intersection([uv, uv2])
 				if !cross.empty():
 					#add verts connected to both v and u
@@ -238,7 +263,7 @@ static func square_to_box(var mdt:MeshDataTool, var size:Vector2, var square:Vec
 		if !b.is_surrounding(uv):
 			outside.append(v)
 	
-	#slide vertices into corners of square
+	#TODO slide vertices into corners of square
 	
 	#get vertex corners of the square
 	var corners:Array = []
@@ -258,14 +283,15 @@ static func square_to_box(var mdt:MeshDataTool, var size:Vector2, var square:Vec
 	outpos.resize(outside.size())
 	for i in outside.size():
 		outpos[i] = mdt.get_vertex(outside[i])
-	debug_positions(node, outpos)
+	#debug_positions(node, outpos)
 	
 	#construct a mesh from inner verts and connected outer verts
 	var st = slice_mesh(mdt, verts.keys(), faces.keys(), verts)
 
 	var m:Mesh = Mesh.new()
 	return st.commit(m)
-	
+
+#convert a square in uv space to a Bound object
 static func square_to_bound(var size:Vector2, var square:Vector2):
 	var center:Vector2 = square_to_uv(size, square)
 	var half_square:Vector2 = Vector2(1 / size.x, 1 / size.y) * 0.5
@@ -347,12 +373,12 @@ static func path_to_mesh(var path:String = "", var debug:bool = false):
 #create a convex or concave shape from a mesh dependant on whether or not the mesh is flat
 static func mesh_to_shape(var m:Mesh):
 	#create the collision shape of the board as a concave shape if and only if the board is not flat
-	#otherwise make a convex shape
-	var shape = m.create_convex_shape()
+	
 	#plane from first face by which to compare every other face and determine if the mesh is flat
 	var flat = true
 	var plane = Plane()
 	
+	#why is this necesary?
 	var mdt = MeshDataTool.new()
 	mdt.create_from_surface(m, 0)
 	#loop through faces until a valid plane is found
@@ -372,9 +398,13 @@ static func mesh_to_shape(var m:Mesh):
 			flat = false
 			break
 	
+	var shape
 	#if mesh is not flat, use a concave shape
 	if !flat:
 		shape = m.create_trimesh_shape()
+	else:
+		#if mesh is not flat, make a convex shape
+		m.create_convex_shape()
 	
 	return shape
 
@@ -397,10 +427,12 @@ static func slice_mesh(var mdt:MeshDataTool, var vertex_indices:PoolIntArray,
 		indexmap[sorted[i]] = i
 		i += 1
 	
+	#go through sorted verts and add them to the surface tool
 	for v in sorted:
 		st.add_normal(mdt.get_vertex_normal(v))
 		st.add_uv(mdt.get_vertex_uv(v))
 		var pos:Vector3
+		#use position dictionary so new positions can be sent in by other functions
 		if v in positions:
 			pos = positions[v]
 		else:
@@ -420,28 +452,36 @@ static func slice_mesh(var mdt:MeshDataTool, var vertex_indices:PoolIntArray,
 static func vert_to_triangle_fan(var mdt:MeshDataTool, var i:int = 0, 
 	var verts:Dictionary = {}, var faces:Dictionary = {}, var edges:bool = false):
 	
+	#get connected faces
 	var fs:Array
 	if edges:
 		fs = mdt.get_edge_faces(i)
 	else:
 		fs = mdt.get_vertex_faces(i)
 	
+	#loop through faces and add their verts
 	for f in fs:
+		#add face to faces dict so calling function can see the faces being searched
 		faces[f] = get_face_vertices(mdt, f)[0]
 		for j in range(0, 3):
 			var v = mdt.get_face_vertex(f, j)
+			#same deal as faces
 			verts[v] = mdt.get_vertex(v)
-			
+	
+	#in case vert_to_triangle_fan doesnt have dictionary args, return them back out
 	return [verts, faces]
 
 #cast out a ray from the camera, given a physics state s
 static func raycast(var p:Vector2, var c:Camera, 
 	var w:World, var v:float = INF, var mask:int = 0x7FFFFFFF):
 	
+	#get physics state of the current scene
 	var s = w.direct_space_state
+	#get ray intersection with that scene
 	var r = s.intersect_ray(
 		c.project_ray_origin(p), c.project_ray_normal(p) * v,
 		[], mask)
+	#if the intersection lands, return r
 	if !r.empty():
 		return r
 	return null
@@ -449,11 +489,13 @@ static func raycast(var p:Vector2, var c:Camera,
 #add CSGsphere children to node at relative positions
 static func debug_positions(var node:Node = null, var positions:PoolVector3Array = [], 
 	var radius:float = 0.1, var material:Material = SpatialMaterial.new()):
-		
+	
+	#remove old debug objects
 	for c in node.get_children():
 		if c.name.find("debug") != -1:
 			node.remove_child(c)
-		
+	
+	#add new ones
 	for p in positions:
 		var csg = CSGSphere.new()
 		csg.radius = radius
